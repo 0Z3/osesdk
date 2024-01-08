f834afecc17742e3c7dc6172e2d3127f70bd7932
John MacCallum <john.m@ccallum.com>
2024-01-08 15:35:44 +0100

    Added a version.h generation target to the makefile

--- makefiles/makefile-module.mk
+++ makefiles/makefile-module.mk
@@ -6,15 +6,25 @@ INCLUDES += -I. -I$(LIBOSEDIR)/..
 
 TARGETNAME ?= o.se.$(BASENAME).so
 
-$(TARGETNAME): $(OTHERDEPS) $(CFILES:.c=.o) $(CXXFILES:.$(CXXEXT)=.o) 
+MACROPREFIX ?= $(BASENAME)
+VARNAME_VERSION ?= $(MACROPREFIX)_version
+VARNAME_DATE_COMPILED ?= $(MACROPREFIX)_date_compiled
+VERSION_H ?= $(BASENAME)_version.h
+
+$(TARGETNAME): $(OTHERDEPS) $(CFILES:.c=.o) $(CXXFILES:.$(CXXEXT)=.o)
+	echo $(VERSION_H)
 	$(CXX) $(LDFLAGS) -o $@ $^ $(OTHERLINKOBJS)
 
-$(CXXFILES:.$(CXXEXT)=.o): %.o: %.$(CXXEXT) $(OTHERCXXFILEDEPS)
+$(CXXFILES:.$(CXXEXT)=.o): %.o: %.$(CXXEXT) $(OTHERCXXFILEDEPS) $(VERSION_H)
 	$(CXX) -c $(CXXFLAGS) $(INCLUDES) $(DEFINES) $< -o $@
 
-$(CFILES:.c=.o): %.o: %.c $(OTHERCFILEDEPS)
+$(CFILES:.c=.o): %.o: %.c $(OTHERCFILEDEPS) $(VERSION_H)
 	$(CC) -c $(CFLAGS) $(INCLUDES) $(DEFINES) $< -o $@
+
+$(VERSION_H):
+	echo "static const char * const $(VARNAME_VERSION) = \""`git describe --always --dirty --tags`"\";" > $(VERSION_H)
+	echo "static const char * const $(VARNAME_DATE_COMPILED) = \""`date`"\";" >> $(VERSION_H)
 
 .PHONY: clean
 clean: $(OTHERCLEANTARGETS)
-	rm -rf *.o *.so *.dSYM $(OTHERCLEAN)
+	rm -rf *.o *.so *.dSYM $(VERSION_H) $(OTHERCLEAN)
